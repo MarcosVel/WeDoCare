fa54681397ff53d7b89fc9d7a3f684db3d7fc52f
MarcosVel <marcos.veloso@sempreceub.com>
2021-04-09 22:46:56 -0300

    complete structure of header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,24 @@
 import './Header.css';
 import Liveclass from '../../assets/liveclass.svg';
 import Meet from '../../assets/Meet.png';
+import Mouse from '../../assets/Mouse.svg';
 
 function index() {
   return (
     <section className="header">
-      <img src={ Liveclass } className="header_logo" />
+      <img src={ Liveclass } alt="Logo liveclass" className="header_logo" />
       <div className="header_content">
         <div className="header_text">
           <h1>Terapia em Grupo</h1>
           <h3>Aprenda a lidar com a culpa e o arrependimento e pare de remoer as possibilidades do futuro.</h3>
           <p>2x por semana, você vai participar de uma sessão de terapia em grupo, acompanhada por um psicólogo, onde vai poder expor e compartilhar suas dores e angústias, dividindo com outras pessoas os problemas da vida cotidiana.</p>
+          <button className="header_btn">Assinar club</button>
         </div>
-        <img src={ Meet } className="header_img" />
+        <img src={ Meet } alt='Imagem Meet' className="header_img" />
       </div>
+      <a href='#'>
+        <img src={ Mouse } alt='Scroll down' className="header_scrollMouse" />
+      </a>
     </section>
   )
 }
