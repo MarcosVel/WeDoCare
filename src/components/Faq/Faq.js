cd1831edc5bc76578f592aa78938457d3e92b043
MarcosVel <marcos.veloso@sempreceub.com>
2021-04-10 04:01:57 -0300

    adjusting Cards

--- src/components/Faq/Faq.js
+++ src/components/Faq/Faq.js
@@ -17,6 +17,15 @@ function Faq() {
               <Card.Body className="faq_txtCardBody">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Card.Body>
             </Accordion.Collapse>
           </Card>
+          <Card className="faq_card">
+            <Accordion.Toggle as={ Card.Header } eventKey="1" className="faq_cardHeader">
+              <img src={ Dropdown } className="faq_dropIcon" />
+              Eu posso fazer só um curso específico ou preciso participar de todos?
+            </Accordion.Toggle>
+            <Accordion.Collapse eventKey="1" className="faq_cardBody">
+              <Card.Body className="faq_txtCardBody">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Card.Body>
+            </Accordion.Collapse>
+          </Card>
         </Accordion>
       </div>
     </section>
