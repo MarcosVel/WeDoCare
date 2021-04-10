e9df0e0f391d8bf10f9218fabb30257dc70058fc
MarcosVel <marcos.veloso@sempreceub.com>
2021-04-09 23:38:59 -0300

    Content finished

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -4,23 +4,20 @@ import Podcast from '../../assets/Podcast.svg';
 
 function Content() {
   return (
-    <section className="content">
-      <div className="content_journal">
+    <section className="content_section">
+      <div className="content">
         <div className="content_text">
           <h3>Emoti Journal</h3>
           <p className="page_text">Ao final de cada ciclo terapêutico, o participante será orientado a registrar em seu diário a evolução de seus sentimentos  e percepções.</p>
         </div>
         <img src={ Journal } alt='Journal' />
       </div>
-      <div className="content_journal">
-        <img src={ Podcast } alt='Journal' />
+      <div className="content">
+        <img src={ Podcast } id='podcast_img' alt='Journal' />
         <div className="content_text">
           <h3>Podcast</h3>
-          <p className="page_text">Ao final de cada ciclo terapêutico, o participante será orientado a registrar em seu diário a evolução de seus sentimentos  e percepções.</p>
+          <p className="page_text">Com temas atuais e que abordam assuntos importantes e determinantes para diversas fases da sua vida, o participante poderá incrementar seu processo terapêutico.</p>
         </div>
-      </div>
-      <div className="content_podcast">
-
       </div>
     </section>
   )
